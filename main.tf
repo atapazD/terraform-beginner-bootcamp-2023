849586e691f01badd5faccbc0c0a8d8ad61e4d2c
Daniel Z <116180321+atapazD@users.noreply.github.com>
2023-10-14 15:59:04 -0400

    #12 Migrate local state to terraform cloud using terraform cloud block (#13)

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+  cloud {
+    organization = "CloudResumeDZ"
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
