4396f72f0fbaa46d80005c9b87d2c98de882bcfd
Daniel Z <116180321+atapazD@users.noreply.github.com>
2023-10-18 10:45:23 -0400

    #24 refactor s3 bucket into the new terrahouse_aws module (#25)

--- main.tf
+++ main.tf
@@ -1,17 +1,5 @@
-
-
-output "random_bucket_name" {
-  value = var.bucket_name
-}
-
-
-#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-resource "aws_s3_bucket" "website_bucket" {
-  #Bucket naming Rules
-  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
-
-  bucket = var.bucket_name
-  tags = {
-    UserUUID = var.user_uuid
-  }
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.user_uuid
+  bucket_name = var.bucket_name
 }
--- variables.tf
+++ variables.tf
@@ -1,17 +1,7 @@
 variable "user_uuid" {
- description = "User UUID in a specific format"
-  validation {
-    condition     = can(regex("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$", var.user_uuid))
-    error_message = "Invalid user UUID format. The UUID should be in the format: 8-4-4-4-12 hexadecimal characters separated by hyphens."
-  }
+  type = string
 }
 
 variable "bucket_name" {
-  type        = string
-  description = "AWS S3 bucket name"
-
-  validation {
-    condition     = can(regex("^[a-z0-9][a-z0-9-]{1,61}[a-z0-9]$", var.bucket_name))
-    error_message = "Invalid bucket name. It should be between 3 and 63 characters, start and end with a lowercase letter or number, and only contain lowercase letters, numbers, and hyphens."
+  type = string
   }
-}
