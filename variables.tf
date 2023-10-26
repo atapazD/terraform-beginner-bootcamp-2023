5d958b036983f1a987577ad3d8f03ead74ea388a
Daniel Z <daniel.zapata1011@gmail.com>
2023-10-26 16:38:04 +0000

    comment

--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
-variable "user_uuid" {
-  type = string
-}
+# variable "user_uuid" {
+#   type = string
+# }
 
-variable "bucket_name" {
-  type = string
-}
+# variable "bucket_name" {
+#   type = string
+# }
 
 
-variable "index_html_filepath" {
-  type = string
+# variable "index_html_filepath" {
+#   type = string
 
-}
+# }
 
-variable "error_html_filepath" {
-  type = string
-}
+# variable "error_html_filepath" {
+#   type = string
+# }
