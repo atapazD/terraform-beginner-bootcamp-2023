e86cbf0c35345c423075820d1e598051c18c2e63
Daniel Z <116180321+atapazD@users.noreply.github.com>
2023-11-08 11:44:57 -0500

    42 terratowns provider (#46)
    * #47 WIP setting up the skeleton for creating customer provider
    
    * #42 wip setting up the skeleton for creating a custom terraform provider
    
    * #42 setup skeleton for resource
    
    * #42 finish implementation of tf provider

--- main.tf
+++ main.tf
@@ -18,7 +18,17 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint  = "http://localhost:4567"
+  endpoint  = "http://localhost:4567/api"
   user_uuid = "e328f4ab-b99f-421c-84c9-4ccea042c7d1"
   token     = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+}
+
+resource "terratowns_home" "home" {
+  name = "Fallout 3!!!"
+  description = <<DESCRIPTION
+  Playing fallout 3 is like getting lost in a new world where the choices you make affect the outcome of the story told.
+  DESCRIPTION
+  domain_name = "3fdq3gz.cloudfront.net"
+  town="gamers-grotto"
+  content_version = 1
 }
